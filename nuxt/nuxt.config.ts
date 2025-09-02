4547ae8c92573775e826904695b0d610b6e23f7b
Simon Brundin <simonbrundin@gmail.com>
2025-09-02 19:41:56 +0200

    added test

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui', 'nuxt-auth-utils']
-})
+  modules: [
+    "@nuxt/image",
+    "@nuxt/ui",
+    "nuxt-auth-utils",
+    "@nuxt/test-utils/module",
+  ],
+});
+
---
+++ nuxt/test/nuxt/hej.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest";
+
+test("adds 1 + 2 to equal 3", () => {
+  let nr: Number = 1;
+  expect(nr).toBe(1);
+});

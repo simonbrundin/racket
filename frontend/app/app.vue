4d6878f1791610f6bb15d03ee9868441e2bbadae
Simon Brundin <simonbrundin@gmail.com>
2025-09-10 17:59:22 +0200

    ddffjhsjdkhfksjdkdkj

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,6 +3,7 @@
     <NuxtRouteAnnouncer />
     <NuxtPage />
     <h1>Hej Emma och Noomi!</h1>
-    Jag älskar er mest i världen <3 Nu ska jag visa en uppdatering
+    <div>Jag älskar er mest i världen <3 Nu ska jag visa en uppdatering</div>
+    <div>version 4</div>
   </div>
 </template>
